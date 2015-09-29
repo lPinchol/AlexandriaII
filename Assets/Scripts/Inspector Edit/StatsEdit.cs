3b18c4182e4a3b3ff06a9c5870e5586abd4716e0
lPinchol <pinchodev91@gmail.com>
2015-09-29 23:38:27 +0200

    Update Inspector Edit

---
+++ Assets/Scripts/Inspector Edit/StatsEdit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsEdit : MonoBehaviour
+{
+    [Header("Name Hero")]
+    [Multiline(2)]
+    [Tooltip("Name of Hero")]
+    public string nameHero;
+
+
+    [Header("Stats Hero")]
+    [Tooltip("Level of Hero")]
+    public int lvlHero;
+
+    [Range(1, 100)]
+    [Tooltip("Xperience of Hero (1-100)")]
+    public float expHero;
+
+    [Tooltip("Type Arm of Hero")]
+    public TypeArm2 typeArm = TypeArm2.Sword;
+}
+
+public enum TypeArm2
+{
+    Sword,
+    Dagge,
+    Staff,
+    Stick
+}
---
+++ Assets/Scripts/Inspector Edit/StatsNormal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsNormal : MonoBehaviour
+{
+    public string nameHero;
+
+    public int lvlHero;
+
+    public float expHero;
+
+    public TypeArm typeArm = TypeArm.Sword;
+}
+
+public enum TypeArm
+{
+    Sword,
+    Dagge,
+    Staff,
+    Stick
+}
